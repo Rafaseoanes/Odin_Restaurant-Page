49b557aed2872ed63736086045555f9b55aec993
rafaseoanes <rafaseoanes9@gmail.com>
2024-07-08 15:48:07 -0500

    Home page setup and style created

--- src/renderHome.js
+++ src/renderHome.js
@@ -1,9 +1,20 @@
-const renderHome = function() {
+import "./homeStyle.css";
+import headImg from "./assets/Designer 2.jpeg";
 
-    const content = document.getElementById("content")
+const renderHome = function () {
+  const content = document.getElementById("content");
 
-    content.innerText = "Rendering home from renderHome!"
+  const headLine = document.createElement("h2");
+  const burgersDescription = document.createElement("h4");
+  const image = document.createElement("img");
 
+  headLine.innerText = "Best burgers in town! Guaranteed.";
+  burgersDescription.innerText =
+    "Delves into the Joy of Burger Craftsmanship, Inspiring Enthusiasts to Savor the Complex Flavors and Rich Aromas in Every Bite.";
+  image.src = headImg;
+
+  // content.innerText = "Rendering home from renderHome!"
+  content.append(image, headLine, burgersDescription);
 };
 
 export default renderHome;
