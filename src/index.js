a167aa17d024e84b3a63027fa7428820e55837e5
rafaseoanes <rafaseoanes9@gmail.com>
2024-07-08 14:49:47 -0500

    Basic structure create to show home page

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 import "./style.css"
+import "./renderHome"
+import renderHome from "./renderHome"
 
+const documentLoad = document.querySelector("body")
+
+documentLoad.addEventListener("load", renderHome())
---
+++ src/renderHome.js
@@ -0,0 +1,9 @@
+const renderHome = function() {
+
+    const content = document.getElementById("content")
+
+    content.innerText = "Rendering home from renderHome!"
+
+};
+
+export default renderHome;
